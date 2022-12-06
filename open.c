080aca24db003e51b067dd22fc21c1b7237660ff
meri-hane <janecalulang@gmail.com>
2022-12-06 12:57:07 +0800

    Fixed format OG

--- open.c
+++ open.c
@@ -1,6 +1,6 @@
 /*
-Implement Open hashing and Closed hashing techniques (of integers) (in C/C++). In closed hashing 
-if collision occurs use any of the 3 techniques (Linear, quadratic and Double hashing). 
+Implement Open hashing and Closed hashing techniques (of integers) (in C/C++). In closed hashing
+if collision occurs use any of the 3 techniques (Linear, quadratic and Double hashing).
 Turn in 2 programs.
 */
 
@@ -8,55 +8,60 @@ Turn in 2 programs.
 #include <stdlib.h>
 #define MAX 10
 
-typedef struct NODE
+typedef struct node
 {
-      int x;
-      struct NODE *next;
+      int data;
+      struct node *next;
 } NODE;
 
-NODE *a[MAX];
+NODE *head[MAX];
+
+void insert();
+void search();
+void display();
 
 void insert()
 {
       int d;
-      printf("Enter the element to be inserted : ");
+      printf("Enter the element to be inserted: ");
       scanf("%d", &d);
       int x = d % MAX;
       NODE *newnode = (NODE *)malloc(sizeof(NODE));
-      newnode->x = d;
+      newnode->data = d;
       newnode->next = NULL;
-      if (a[x] == NULL)
-            a[x] = newnode;
+      if (head[x] == NULL)
+            head[x] = newnode;
       else
       {
             NODE *ptr;
-            ptr = a[x];
+            ptr = head[x];
             int flag = 1;
             while (ptr->next != NULL)
             {
-                  if (ptr->x == d)
+                  if (ptr->data == d)
                         flag = 0;
                   ptr = ptr->next;
             }
-            if (ptr->x == d)
+            if (ptr->data == d)
                   flag = 0;
             if (flag == 1)
                   ptr->next = newnode;
             else
-                  printf("Element already exists !");
+                  printf("Element already exists!");
       }
 }
+
 void search()
 {
-      printf("Enter the element you want to search :");
+      printf("Enter the element you want to search: ");
       int d;
       scanf("%d", &d);
       int x = d % MAX;
-      NODE *ptr = a[x];
+      NODE *ptr = head[x];
       int flag = 0;
       while (ptr != NULL)
       {
-            if (ptr->x == d)
+            if (ptr->data == d)
                   flag = 1;
             ptr = ptr->next;
       }
@@ -65,15 +70,17 @@ void search()
       else
             printf("Element not found");
 }
+
 void display()
 {
+      printf("\n");
       for (int i = 0; i < MAX; i++)
       {
-            NODE *ptr = a[i];
-            printf("Index %d - ", i);
+            NODE *ptr = head[i];
+            printf("Bucket %d - ", i);
             while (ptr != NULL)
             {
-                  printf("%d, ", ptr->x);
+                  printf("%d, ", ptr->data);
                   ptr = ptr->next;
             }
             printf("\n");
@@ -82,11 +89,11 @@ void display()
 int main()
 {
       for (int i = 0; i < MAX; i++)
-            a[i] = NULL;
+            head[i] = NULL;
       printf("1. Insert\n2. Search\n3. Display\n4. Exit");
       while (1)
       {
-            printf("\nEnter your choice :");
+            printf("\nEnter your choice: ");
             int ch;
             scanf("%d", &ch);
             switch (ch)
@@ -103,7 +110,7 @@ int main()
             case 4:
                   exit(1);
             default:
-                  printf("Wrong choice\n");
+                  printf("Invalid\n");
             }
       }
 }
